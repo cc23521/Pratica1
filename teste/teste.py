ee3b8ff0e3e82a85123ea98fc79bc54a445c21c6
Vitor Coelho <vijcoelho@gmail.com>
2023-06-20 00:11:43 -0300

    up

--- teste/teste.py
+++ teste/teste.py
@@ -66,8 +66,11 @@ class Ui_InsertStudentWindow(object):
         self.pushButton_back.setGeometry(QtCore.QRect(50, 240, 100, 30))
         self.pushButton_back.setObjectName("pushButton_back")
         self.pushButton_cancel = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_cancel.setGeometry(QtCore.QRect(250, 240, 100, 30))
+        self.pushButton_cancel.setGeometry(QtCore.QRect(150, 240, 100, 30))
         self.pushButton_cancel.setObjectName("pushButton_cancel")
+        self.pushButton_save = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_save.setGeometry(QtCore.QRect(250, 240, 100, 30))
+        self.pushButton_save.setObjectName("pushButton_save")
         self.label_name = QtWidgets.QLabel(self.centralwidget)
         self.label_name.setGeometry(QtCore.QRect(50, 30, 100, 30))
         self.label_name.setObjectName("label_name")
@@ -108,6 +111,7 @@ class Ui_InsertStudentWindow(object):
         InsertStudentWindow.setWindowTitle(_translate("InsertStudentWindow", "Insert Student"))
         self.pushButton_back.setText(_translate("InsertStudentWindow", "Back"))
         self.pushButton_cancel.setText(_translate("InsertStudentWindow", "Cancel"))
+        self.pushButton_save.setText(_translate("InsertApontamentoWindow", "Save"))
         self.label_name.setText(_translate("InsertStudentWindow", "Name:"))
         self.label_ra.setText(_translate("InsertStudentWindow", "RA:"))
         self.label_email.setText(_translate("InsertStudentWindow", "Email:"))
@@ -124,8 +128,11 @@ class Ui_InsertPropertyWindow(object):
         self.pushButton_back.setGeometry(QtCore.QRect(50, 240, 100, 30))
         self.pushButton_back.setObjectName("pushButton_back")
         self.pushButton_cancel = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_cancel.setGeometry(QtCore.QRect(250, 240, 100, 30))
+        self.pushButton_cancel.setGeometry(QtCore.QRect(150, 240, 100, 30))
         self.pushButton_cancel.setObjectName("pushButton_cancel")
+        self.pushButton_save = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_save.setGeometry(QtCore.QRect(250, 240, 100, 30))
+        self.pushButton_save.setObjectName("pushButton_save")
         self.label_Idimovel = QtWidgets.QLabel(self.centralwidget)
         self.label_Idimovel.setGeometry(QtCore.QRect(50, 30, 100, 30))
         self.label_Idimovel.setObjectName("label_Idimovel")
@@ -148,8 +155,64 @@ class Ui_InsertPropertyWindow(object):
         InsertPropertyWindow.setWindowTitle(_translate("InsertPropertyWindow", "Insert Property"))
         self.pushButton_back.setText(_translate("InsertPropertyWindow", "Back"))
         self.pushButton_cancel.setText(_translate("InsertPropertyWindow", "Cancel"))
-        self.label_Idimovel.setText(_translate("InsertPropertyWindow", "Id_imovel:"))
-        self.label_bloco.setText(_translate("InsertPropertyWindow", "Bloco:"))
+        self.pushButton_save.setText(_translate("InsertApontamentoWindow", "Save"))
+        self.label_Idimovel.setText(_translate("InsertPropertyWindow", "Id_Property:"))
+        self.label_bloco.setText(_translate("InsertPropertyWindow", "Apartment block:"))
+
+class Ui_InsertApontamentoWindow(object):
+    def setupUi(self, InsertApontamentoWindow):
+        InsertApontamentoWindow.setObjectName("InsertApontamentoWindow")
+        InsertApontamentoWindow.resize(400, 300)
+        self.centralwidget = QtWidgets.QWidget(InsertApontamentoWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton_back = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_back.setGeometry(QtCore.QRect(50, 240, 100, 30))
+        self.pushButton_back.setObjectName("pushButton_back")
+        self.pushButton_cancel = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_cancel.setGeometry(QtCore.QRect(150, 240, 100, 30))
+        self.pushButton_cancel.setObjectName("pushButton_cancel")
+        self.pushButton_save = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_save.setGeometry(QtCore.QRect(250, 240, 100, 30))
+        self.pushButton_save.setObjectName("pushButton_save")
+        self.label_idApontamento = QtWidgets.QLabel(self.centralwidget)
+        self.label_idApontamento.setGeometry(QtCore.QRect(50, 30, 100, 30))
+        self.label_idApontamento.setObjectName("label_idApontamento")
+        self.lineEdit_idApontamento = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_idApontamento.setGeometry(QtCore.QRect(150, 30, 200, 30))
+        self.lineEdit_idApontamento.setObjectName("lineEdit_idApontamento")
+        self.label_RA = QtWidgets.QLabel(self.centralwidget)
+        self.label_RA.setGeometry(QtCore.QRect(50, 70, 100, 30))
+        self.label_RA.setObjectName("label_RA")
+        self.lineEdit_RA = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_RA.setGeometry(QtCore.QRect(150, 70, 200, 30))
+        self.lineEdit_RA.setObjectName("lineEdit_RA")
+        self.label_entrada = QtWidgets.QLabel(self.centralwidget)
+        self.label_entrada.setGeometry(QtCore.QRect(50, 110, 200, 30))
+        self.label_entrada.setObjectName("linelabel_entrada")
+        self.lineEdit_entrada = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_entrada.setGeometry(QtCore.QRect(150, 110, 200, 30))
+        self.lineEdit_entrada.setObjectName("lineEdit_entrada")
+        self.label_saida = QtWidgets.QLabel(self.centralwidget)
+        self.label_saida.setGeometry(QtCore.QRect(50, 150, 200, 30))
+        self.label_saida.setObjectName("linelabel_saida")
+        self.lineEdit_saida = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_saida.setGeometry(QtCore.QRect(150, 150, 200, 30))
+        self.lineEdit_saida.setObjectName("lineEdit_saida")
+        InsertApontamentoWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(InsertApontamentoWindow)
+        QtCore.QMetaObject.connectSlotsByName(InsertApontamentoWindow)
+
+    def retranslateUi(self, InsertApontamentoWindow):
+        _translate = QtCore.QCoreApplication.translate
+        InsertApontamentoWindow.setWindowTitle(_translate("InsertApontamentoWindow", "Insert Apontamento"))
+        self.pushButton_back.setText(_translate("InsertApontamentoWindow", "Back"))
+        self.pushButton_cancel.setText(_translate("InsertApontamentoWindow", "Cancel"))
+        self.pushButton_save.setText(_translate("InsertApontamentoWindow", "Save"))
+        self.label_idApontamento.setText(_translate("InsertApontamentoWindow", "Id_Notes:"))
+        self.label_RA.setText(_translate("InsertApontamentoWindow", "RA:"))
+        self.label_entrada.setText(_translate("InsertApontamentoWindow", "Entry:"))
+        self.label_saida.setText(_translate("InsertApontamentoWindow", "Exit:"))
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -161,6 +224,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.pushButton_delete_student.clicked.connect(self.delete_student)
         self.pushButton_insert_property.clicked.connect(self.open_insert_property_window)
         self.pushButton_delete_property.clicked.connect(self.delete_property)
+        self.pushButton_insert_apontamento.clicked.connect(self.open_insert_apontamento_window)
+        self.pushButton_delete_apontamento.clicked.connect(self.delete_apontamento)
 
     def open_insert_student_window(self):
         self.insert_student_window = QtWidgets.QMainWindow()
@@ -169,6 +234,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.insert_student_window.show()
         self.ui.pushButton_back.clicked.connect(self.close_insert_student_window)
         self.ui.pushButton_cancel.clicked.connect(self.clear_insert_student_fields)
+        self.ui.pushButton_save.clicked.connect(self.save_data_estudante)
 
     def close_insert_student_window(self):
         self.insert_student_window.close()
@@ -192,6 +258,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.insert_property_window.show()
         self.ui.pushButton_back.clicked.connect(self.close_insert_property_window)
         self.ui.pushButton_cancel.clicked.connect(self.clear_insert_property_fields)
+        self.ui.pushButton_save.clicked.connect(self.save_data_imovel)
 
     def close_insert_property_window(self):
         self.insert_property_window.close()
@@ -205,6 +272,56 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.lineEdit_Idimovel.clear()
         self.ui.lineEdit_bloco.clear()
 
+    def open_insert_apontamento_window(self):
+        self.insert_apontamento_window = QtWidgets.QMainWindow()
+        self.ui = Ui_InsertApontamentoWindow()
+        self.ui.setupUi(self.insert_apontamento_window)
+        self.insert_apontamento_window.show()
+        self.ui.pushButton_back.clicked.connect(self.close_insert_apontamento_window)
+        self.ui.pushButton_cancel.clicked.connect(self.clear_insert_apontamento_fields)
+        self.ui.pushButton_save.clicked.connect(self.save_data_apontamento)
+
+    def close_insert_apontamento_window(self):
+        self.insert_apontamento_window.close()
+
+    def delete_apontamento(self):
+        idApontamento, ok = QtWidgets.QInputDialog.getText(self, "Delete Apontamento", "Enter IdApontamento:")
+        if ok and idApontamento:
+            print("Apontamento deleted:", idApontamento)
+
+    def clear_insert_apontamento_fields(self):
+        self.ui.lineEdit_idApontamento.clear()
+        self.ui.lineEdit_RA.clear()
+        self.ui.lineEdit_entrada.clear()
+        self.ui.lineEdit_saida.clear()
+
+    def save_data_apontamento(self):
+        idApontamento = self.ui.lineEdit_idApontamento.text()
+        ra = self.ui.lineEdit_RA.text()
+        entrada = self.ui.lineEdit_entrada.text()
+        saida = self.ui.lineEdit_saida.text()
+        print("ID Apontamento:", idApontamento)
+        print("RA:", ra)
+        print("Entrada:", entrada)
+        print("Saída:", saida)
+        
+    def save_data_estudante(self):
+        name = self.ui.lineEdit_name.text()
+        ra = self.ui.lineEdit_ra.text()
+        email = self.ui.lineEdit_email.text()
+        property_id = self.ui.lineEdit_property_id.text()
+        absent_days = self.ui.label_absent_days.text()
+        print("Name:", name)
+        print("RA:", ra)
+        print("Email:", email)
+        print("Property Id:", property_id)
+        print("Absent days:", absent_days)
+        
+    def save_data_imovel(self):
+        idImovel = self.ui.lineEdit_Idimovel.text()
+        bloco = self.ui.lineEdit_bloco.text()
+        print("Id Imovel:", idImovel)
+        print("Bloco:", bloco)
 
 if __name__ == "__main__":
     import sys
