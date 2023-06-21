c9e5f35053b0344eb7d32a9de4c3a5359d61e95f
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2023-06-21 15:59:26 -0300

    Update main.py

--- cli/main.py
+++ cli/main.py
@@ -51,6 +51,39 @@ def atualizar_vagas_imovel(idImovel):
     except bd.Error as err:
         print("Não foi possível atualizar número de vagas:", err)
 
+def listar_imovel():
+    meuCursor = conexao.cursor()
+    listar_todos = "foo"
+    while listar_todos != '':
+        listar_todos = input("Listar todos imóves [s/n]? (Enter para sair) ").upper()
+        if listar_todos not in ('S', 'N'): continue
+        
+        if listar_todos == 'S':
+            try:
+                meuCursor.execute("select * from timeB.Imovel")
+                imoveis = meuCursor.fetchall()
+                print("idImovel,bloco,vagas")
+                for imovel in imoveis:
+                    idImovel, bloco, vagas = imovel[0], imovel[1], imovel[2]
+                    print(idImovel, bloco, vagas, sep=',')
+                print()
+            except bd.Error as err:
+                print("Não foi possível listar os imóveis:", err)
+        else:
+            idImovel = input("Id Imovel: ")
+            try:
+                sComando = "select * from timeB.Imovel where idImovel = (?)"
+                meuCursor.execute(sComando, idImovel)
+                imovel = meuCursor.fetchall()[0]
+                print("idImovel,bloco,vagas")
+                for col in imovel:
+                    print(col, end=',')
+                print("\n")
+            except bd.Error as err:
+                print("Não foi possível listar o imóvel:", err)
+            except IndexError:
+                print("Imóvel não está registrado.")
+
 def tem_vagas():
     meuCursor = conexao.cursor()
     try:
@@ -64,26 +97,10 @@ def tem_vagas():
     except bd.Error as err:
         print(err)
 
-def listar_imoveis():
-    meuCursor = conexao.cursor()
-    try:
-        meuCursor.execute("select * from timeB.Imovel")
-
-        imoveis = meuCursor.fetchall()
-        print("idImovel,bloco,vagas")
-        for imovel in imoveis:
-            idImovel, bloco, vagas = imovel[0], imovel[1], imovel[2]
-            if vagas > 0:
-                print(idImovel, bloco, vagas, sep=",")
-    except bd.Error as err:
-        print("Não foi possível listar os imóveis:", err)
-
 def incluir_estudante():
     meuCursor = conexao.cursor()
     ra = "foo"
     while ra != '':
-        listar_estudante()
-        print()
         if not tem_vagas(): break
 
         ra = input("RA (Enter para terminar): ")
@@ -93,7 +110,7 @@ def incluir_estudante():
         email = input("Email: ")
         nomeSocial = input("Nome social (Enter para pular): ")
         print("\nImóveis disponíveis")
-        listar_imoveis()
+        listar_imovel()
         idImovel = input("\nId Imóvel: ")
         sComando = "insert into timeB.Estudante (RA, email, nomeSocial, idImovel) values (?, ?, ?, ?)"
         try:
@@ -103,23 +120,59 @@ def incluir_estudante():
             print("Não foi possível incluir estudante:", err)    
     meuCursor.commit()
 
+def excluir_estudante():
+    meuCursor = conexao.cursor()
+    ra = "foo"
+    while ra != '':
+        ra = input("RA (Enter para terminar): ")
+
+        if ra == '': continue
+    
+        try:
+            meuCursor.execute("DELETE FROM timeB.Apontamento WHERE RA = ?", ra)
+            meuCursor.execute("DELETE FROM timeB.Estudante WHERE RA = ?", ra)
+            print("Registros de estudante excluídos com sucesso!")
+        except bd.Error as err:
+            print("Ocorreu um erro ao excluir os registros:", err)
+    meuCursor.commit()   
+
 def listar_estudante():
     meuCursor = conexao.cursor()
-    try:
-        meuCursor.execute("select * from timeB.Estudante")
-
-        estudantes = meuCursor.fetchall()
-        print("RA,email,nomeSocial,semestreAtual,diasAusente,idImovel")
-        for estudante in estudantes:
-            ra =            estudante[0]
-            email =         estudante[1]
-            nomeSocial =    estudante[2]
-            semestreAtual = estudante[3]
-            diasAusente =   estudante[4]
-            idImovel =      estudante[5]
-            print(ra, email, nomeSocial, semestreAtual, diasAusente, idImovel, sep=",")
-    except bd.Error as err:
-        print("Não foi possível listar os imóveis:", err)
+    listar_todos = "foo"
+    while listar_todos != '':
+        listar_todos = input("Listar todos estudantes [s/n]? (Enter para sair) ").upper()
+        if listar_todos not in ('S', 'N'): continue
+        
+        if listar_todos == 'S':
+            try:
+                meuCursor.execute("select * from timeB.Estudante")
+                estudantes = meuCursor.fetchall()
+                print("RA,email,nomeSocial,semestreAtual,diasAusente,idImovel")
+                for estudante in estudantes:
+                    ra =            estudante[0]
+                    email =         estudante[1]
+                    nomeSocial =    estudante[2]
+                    semestreAtual = estudante[3]
+                    diasAusente =   estudante[4]
+                    idImovel =      estudante[5]
+                    print(ra, email, nomeSocial, semestreAtual, diasAusente, idImovel, sep=',')
+                print()
+            except bd.Error as err:
+                print("Não foi possível listar os estudantes:", err)
+        else:
+            ra = input("RA: ")
+            try:
+                sComando = "select * from timeB.Estudante where RA = (?)"
+                meuCursor.execute(sComando, ra)
+                estudante = meuCursor.fetchall()[0]
+                print("RA,email,nomeSocial,semestreAtual,diasAusente,idImovel")
+                for col in estudante:
+                    print(col, end=',')
+                print("\n")
+            except bd.Error as err:
+                print("Não foi possível listar o imóvel:", err)
+            except IndexError:
+                print("Estudante não está registrado.")
 
 def incluir_apontamento():
     meuCursor = conexao.cursor()
@@ -136,7 +189,55 @@ def incluir_apontamento():
         except bd.Error as err:
             print("Não foi possível incluir apontamento:", err)    
     meuCursor.commit()
+
+def listar_apontamento():
+    meuCursor = conexao.cursor()
+    listar_todos = "foo"
+    while listar_todos != '':
+        listar_todos = input("Listar todos apontamentos [s/n]? (Enter para sair) ").upper()
+        if listar_todos not in ('S', 'N'): continue
+        
+        if listar_todos == 'S':
+            try:
+                meuCursor.execute("select * from timeB.Apontamento")
+                apontamentos = meuCursor.fetchall()
+                print("idApontamento,RA,entrada,saída")
+                for apontamento in apontamentos:
+                    idApontamento, ra, entrada, saida = apontamento[0], apontamento[1], apontamento[2], apontamento[3]
+                    print(idApontamento, ra, entrada, saida, sep=',')
+                print()
+            except bd.Error as err:
+                print("Não foi possível listar os apontamentos:", err)
+        else:
+            idApontamento = input("Id Apontamento: ")
+            try:
+                sComando = "select * from timeB.Apontamento where idApontamento = (?)"
+                meuCursor.execute(sComando, idApontamento)
+                apontamento = meuCursor.fetchall()[0]
+                print("idApontamento,RA,entrada,saída")
+                for col in apontamento:
+                    print(col, end=',')
+                print("\n")
+            except bd.Error as err:
+                print("Não foi possível listar o apontamento:", err)
+            except IndexError:
+                print("Apontamento não está registrado.")
+
+def excluir_apontamento():
+    meuCursor = conexao.cursor()
+    idApontamento = "foo"
+    while idApontamento != '':
+        idApontamento = input("Id Apontamento (Enter para terminar): ")
+
+        if idApontamento == '': continue
     
+        try:
+            meuCursor.execute("DELETE FROM timeB.Apontamento WHERE idApontamento = ?", idApontamento)
+            print("Registro de apontamento excluído com sucesso!")
+        except bd.Error as err:
+            print("Ocorreu um erro ao excluir os registros:", err)
+    meuCursor.commit()
+
 def incluir_registro():
     opcao = 'foo'
     while opcao not in ('0', ''):
@@ -156,37 +257,6 @@ def incluir_registro():
             case '2': incluir_estudante()
             case '3': incluir_apontamento()
 
-def excluir_estudante():
-    meuCursor = conexao.cursor()
-    ra = "foo"
-    while ra != '':
-        ra = input("RA (Enter para terminar): ")
-
-        if ra == '': continue
-    
-        try:
-            meuCursor.execute("DELETE FROM timeB.Apontamento WHERE RA = ?", ra)
-            meuCursor.execute("DELETE FROM timeB.Estudante WHERE RA = ?", ra)
-            print("Registros excluídos com sucesso!")
-        except bd.Error as err:
-            print("Ocorreu um erro ao excluir os registros:", err)
-    meuCursor.commit()    
-
-def excluir_imovel():
-    meuCursor = conexao.cursor()
-    idImovel = 'foo'
-    while idImovel not in ('0', ''):
-        idImovel = input("Id Imóvel (Enter para terminar): ")
-
-        if idImovel in ('0', ''): continue
-
-        try:
-            meuCursor.execute("delete from timeB.Imovel where idImovel = ?", idImovel)
-            print("Registros excluídos com sucesso!")
-        except bd.Error as err:
-            print("Ocorre um erro ao excluir o registro:", err)
-    meuCursor.commit()
-
 def excluir_registro():
     opcao = '1'
     while opcao not in ('0', ''):
@@ -195,13 +265,11 @@ def excluir_registro():
         print("=" * 9, "=" * 11, "\n")
         print("0 - Voltar")
         print("1 - Excluir Estudante")
-        print("2 - Excluir Imóvel")
+        print("2 - Excluir Apontamento")
         opcao = input("\nDigite o número da opção desejada: ")
         match opcao:
             case '1': excluir_estudante()
-            case '2': excluir_imovel()
-
-def alterar_registro():
+            case '2': excluir_apontamento()
     meuCursor = conexao.cursor()
     ra = "12345"
     while ra != '':
@@ -224,6 +292,22 @@ def alterar_registro():
             except bd.Error as err:
                 print("Ocorreu um erro ao alterar o registro:", err)
 
+def listar_registro():
+    opcao = '1'
+    while opcao not in ('0', ''):
+        os.system("cls")
+        print("Operações disponíveis")
+        print("=" * 9, "=" * 11, "\n")
+        print("0 - Voltar")
+        print("1 - Listar Imóvel")
+        print("2 - Listar Estudante")
+        print("3 - Listar Apontamento")
+        opcao = input("\nOpção desejada: ")
+        match opcao:
+            case '1': listar_imovel()
+            case '2': listar_estudante()
+            case '3': listar_apontamento()
+
 def seletor():
     opcao = '1'
     while opcao not in ('0', ''):
@@ -233,14 +317,12 @@ def seletor():
         print("0 - Sair")
         print("1 - Incluir Registro")
         print("2 - Excluir Registro")
-        print("3 - Alterar Registro")
-        print("4 - Listar Registro")
+        print("3 - Listar Registro")
         opcao = input("\nOpção desejada: ")
         match opcao:
             case '1': incluir_registro()
             case '2': excluir_registro()
-            # case 3: alterar_registro()
-            # case 4: listar_registro()
+            case '3': listar_registro()
 
 if conectou():
     seletor()
